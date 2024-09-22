0044490c55c97a12204a201f2ed160e679c43842
zahra121512 <jabbar1215@yahoo.com>
2024-09-22 12:12:31 +1000

    yes

--- act2.py
+++ act2.py
@@ -1,9 +1,9 @@
 Amount =int(input("Please Enter Amount for Withdraw :"))
 
-note1 = Amount//100
-note2 = (Amount%100)//50
-note3 = ((Amount%100)%50)//10
+note1 = Amount%10
+note2 = (Amount%100)%10
+note3 = ((Amount%100)  %50) %10
 
-print( "notes of 100 rupee" , note1)
-print( "notes of 50 rupee" , note2)
-print( "notes of 10 rupee" , note3)
+print( "notes of 100 squares" , note1)
+print( "notes of 50 squares" , note2)
+print( "notes of 10 squares" , note3)
